2b1c84b2a6689ef84ba186c5260969e0f0c8af54
elyzinger <45950112+elyzinger@users.noreply.github.com>
2018-12-25 20:17:26 +0200

    Add files via upload

---
+++ if.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			Console.WriteLine("pick 2 num");
+			int x = Convert.ToInt32(Console.ReadLine());
+			int y = Convert.ToInt32(Console.ReadLine());
+			if (x > y)
+
+			{ Console.WriteLine(x); }
+			else
+			{ Console.WriteLine(y); }
+
+			
+		}
+	}
+}
